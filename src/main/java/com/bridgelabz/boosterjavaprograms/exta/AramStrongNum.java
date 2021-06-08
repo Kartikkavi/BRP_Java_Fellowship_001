d488465bcf2438e28d759a00e0f60504cdb21cc3
kartik <kavimandankartik33@gmail.com>
2021-06-08 13:12:05 +0530

    Added : Aramstrong number

--- src/main/java/com/bridgelabz/boosterjavaprograms/exta/AramStrongNum.java
+++ src/main/java/com/bridgelabz/boosterjavaprograms/exta/AramStrongNum.java
@@ -1,10 +1,11 @@
+//Program to check the Nuber is Armstrong or not
 package com.bridgelabz.boosterjavaprograms.exta;
 
 import java.util.Scanner;
 
 public class AramStrongNum {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number");
         int num = sc.nextInt();
@@ -13,6 +14,7 @@ public class AramStrongNum {
         //It is the number to check armstrong
         temp = num;
         while (num > 0) {
+            //if the sum of cube of all number is equal to that orignal numbber the is print the number is Aramstrong
             a = num % 10;
             num = num / 10;
             c = c + (a * a * a);
