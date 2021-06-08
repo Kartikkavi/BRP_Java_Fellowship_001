7c69fb0ed6f68bcc17f5a0765bf4b555a7502ecb
kartik <kavimandankartik33@gmail.com>
2021-06-08 17:43:35 +0530

    Modified: convert Num to Word

--- src/main/java/com/bridgelabz/boosterjavaprograms/exta/NumToWord.java
+++ src/main/java/com/bridgelabz/boosterjavaprograms/exta/NumToWord.java
@@ -4,29 +4,40 @@ import java.util.Scanner;
 
 public class NumToWord {
     //By the help of static we are calling the array without creating object.
-    public static void main(String[] args) {
 
+
+    public static final String[] units = {"zero", "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine", "ten", "elevel", "twelve", "thirteen", "fourteen",
+            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+    public static final String[] tens = new String[]{"", "", "twenty", "thirty", "fourty", "fifity",
+            "sixty", "seventy", "eighty", "ninety"};
+
+    public static String convert(int num) {
+        if (num < 0) {
+            return "Minus" + convert(-num);
+        }
+
+
+        if (num < 20) {
+            return units[num];
+        }
+        if (num < 100) {
+            return tens[num / 10] + ((num % 10 != 0) ? "  " : "  ") + units[num % 10];
+        } else if (num < 1000) {
+            return units[num / 100] + " Hundred and" + ((num % 100 != 0) ? " " : " ") + convert(num % 100);
+        }
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+int num;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Num");
-        int num=sc.nextInt();
-
-
-        String [] units = new String[]{"zero", "one", "two", "three", "four", "five",
-                "six", "seven", "eight", "nine", "ten", "elevel", "twelve", "thirteen", "fourteen",
-                "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-
-        String[] tens=new String[] {"", "ten", "twenty", "thirty", "fourty", "fifity",
-                "sixty", "seventy", "eighty", "ninety"};
-        if(num < 20){
-            System.out.println("Minus" + (-num));
-        }
-        if(num < 100){
-            System.out.println(tens[num/10] + ((num%10!=0)? "  "  : "  ") + units[num%10]);
-        }
-        if (num<100){
-            System.out.println(units[num/100] + "Hundred"+ ((num%100!=0)? "  "  : "  ") + units[num%100]);
-
-        }
+         num = sc.nextInt();
+         System.out.println(convert(num));
 
     }
 }
