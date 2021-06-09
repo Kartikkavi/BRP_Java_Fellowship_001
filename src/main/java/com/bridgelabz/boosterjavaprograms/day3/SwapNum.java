71e4c81b40a9396a519b0c4cd4886f7fbc0e8a28
kartik <kavimandankartik33@gmail.com>
2021-06-09 09:08:14 +0530

    Added: 1st and 2nd Smallest ang largest num from Array

--- src/main/java/com/bridgelabz/boosterjavaprograms/day3/SwapNum.java
+++ src/main/java/com/bridgelabz/boosterjavaprograms/day3/SwapNum.java
@@ -1,37 +1,23 @@
 package com.bridgelabz.boosterjavaprograms.day3;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class SwapNum {
 
     public static void main(String[] args) {
-        //Swap an array
-        int [] arr={1,2,3,4,5,6};
-        System.out.println(Arrays.toString(arr));
-        int i=0;
-        int j =0;
-        int temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-       // for (){
-
-        }
 
 
-
-
-    }
-
-// Swap two num
-
-//        int a,b,c;
+        //       Swap two num
+//
+//        int a, b, c;
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter Num");
-//         a = sc.nextInt();
-//         b =sc.nextInt();
+//        a = sc.nextInt();
+//        b = sc.nextInt();
 //        System.out.println("Swaping num");
-//        c=a;
-//        a=b;
-//        b=c;
-//        System.out.println("after Swaping   "+a+"      "+b);
+//        c = a;  //Assining the value of a to c, d to a and c to a.
+//        a = b;
+//        b = c;
+//        System.out.println("after Swaping   " + a + "      " + b);
+     }
+}
---
+++ src/main/java/com/bridgelabz/boosterjavaprograms/exta/LargestNum.java
@@ -0,0 +1,44 @@
+package com.bridgelabz.boosterjavaprograms.exta;
+
+public class LargestNum {
+    public static void main(String[] args) {
+
+        int array[] = {10, 20, 25, 63, 96, 57};
+        int smalest =array[0];
+        int secondsmalest = array[1];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] <smalest)
+
+            {
+                secondsmalest = smalest;
+                smalest = array[i];
+            }
+            else if(array[i] < secondsmalest && array[i]!=smalest)
+            {
+                secondsmalest=array[i];
+            }
+        }
+        System.out.println("2nd smallest value"+secondsmalest);
+        System.out.println("1st smallest value"+smalest);
+
+//
+//        int array[] = {10, 20, 25, 63, 96, 57};
+//        int largest =array[0];
+//        int secondlargest = array[1];
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] > largest)
+//
+//            {
+//                secondlargest = largest;
+//                largest = array[i];
+//            }
+//            else if(array[i] > secondlargest && array[i]!=largest)
+//            {
+//                secondlargest=array[i];
+//            }
+//        }
+//        System.out.println("2nd largest value"+secondlargest);
+//        System.out.println("1st largest value"+largest);
+
+    }
+}
