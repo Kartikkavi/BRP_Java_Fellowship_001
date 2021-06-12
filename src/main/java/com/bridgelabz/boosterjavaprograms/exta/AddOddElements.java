1d993b2004a3325904bbe710d893a0023141b3de
kartik <kavimandankartik33@gmail.com>
2021-06-12 08:36:58 +0530

    Added: Add odd position num and reverse the num

--- src/main/java/com/bridgelabz/boosterjavaprograms/exta/AddOddElements.java
+++ src/main/java/com/bridgelabz/boosterjavaprograms/exta/AddOddElements.java
@@ -4,18 +4,27 @@ import java.util.Arrays;
 
 public class AddOddElements {
     public static void main(String[] args) {
-        int a=0;
-        int[] arr = new int[]{1,2,3,4,5,6,7,8,9};
+      int  reverse=0;
+        int a = 0;
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println(Arrays.toString(arr));
 
         //TO get element at odd position
-        for (int i =0;i <arr.length;i=i+2){
-            a = arr[i]+a;
+        for (int i = 0; i < arr.length; i = i + 2) {
+            a = arr[i] + a;
             System.out.println(arr[i]);
         }
         System.out.println(a);
+
+        //Reversed the a
+        while (a != 0) {
+            //Reversing the Number.
+            int remainder = a % 10;
+            reverse = reverse * 10 + remainder;
+            a = a / 10;
+        }
+        System.out.println("Reversethe Num ============= " + reverse);
+
+
     }
-    //Reversed the a
-
-
 }
